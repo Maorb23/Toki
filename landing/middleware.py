2f17bc6ac0aa59c3530093833035f40fa4e86d00
Maorb23 <maorblumberg@gmail.com>
2026-01-28 00:25:19 +0200

    Adding middleware

--- landing/middleware.py
+++ landing/middleware.py
@@ -10,3 +10,18 @@ class PrintExceptionMiddleware:
             import traceback, sys
             traceback.print_exc(file=sys.stderr)
             raise
+
+# landing/middleware.py
+from django.http import HttpResponseBadRequest
+from django.core.exceptions import DisallowedHost
+
+class DebugHostMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        try:
+            return self.get_response(request)
+        except DisallowedHost:
+            host = request.get_host()
+            return HttpResponseBadRequest(f"DisallowedHost: {host}")
